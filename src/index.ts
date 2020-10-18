5486c0265428b6c89b0369e83e90fcfddffb1ff7
guptaharsh25 <me@harshgupta.ca>
2020-10-18 13:43:40 -0400

    auto build setup

--- src/index.ts
+++ src/index.ts
@@ -48,5 +48,5 @@ app.post( "/newPerson", ( req, res ) => {
 // start the Express server
 app.listen( port, () => {
     // tslint:disable-next-line:no-console
-    console.log( `server started at http://localhost:${ port }` );
+    console.log(`server started at http://localhost:${port}`);
 } );
