db99aa8f6a4347576e443eee0c66bbd443705ca3
Daniel Shwan <dannyshwan@gmail.com>
2021-02-24 02:20:45 -0500

    edit schema for ui overhaul

--- src/model/schema/person.ts
+++ src/model/schema/person.ts
@@ -1,20 +1,23 @@
-import { Schema } from "mongoose";
-import mongoose from "mongoose";
+import { Schema } from 'mongoose';
+import mongoose from 'mongoose';
 
 const person = new mongoose.Schema({
-    time: {
-        type: mongoose.Schema.Types.String
-        },
-    score: {
-        type: mongoose.Schema.Types.String
-        },
-    mask_status: {
-            type: mongoose.Schema.Types.String
-            },
-    photo: {
-        type: mongoose.Schema.Types.Array
-        },
+	time: {
+		type: mongoose.Schema.Types.String,
+	},
+	score: {
+		type: mongoose.Schema.Types.String,
+	},
+	mask_status: {
+		type: mongoose.Schema.Types.String,
+	},
+	photo: {
+		type: mongoose.Schema.Types.Array,
+	},
+});
 
+person.set('toJSON', {
+	virtuals: true,
 });
 
 export default person;
